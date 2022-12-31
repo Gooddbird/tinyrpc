bb5b8554da2dc28b77b67b729691d8823fb664ae
Gooddbird <1753009868@qq.com>
2022-12-31 15:11:00 +0800

    finish corutine_mutex

--- testcases/test_tinypb_server.cc
+++ testcases/test_tinypb_server.cc
@@ -4,18 +4,15 @@
 
 #include "tinyrpc/net/tcp/tcp_server.h"
 #include "tinyrpc/net/net_address.h"
+#include "tinyrpc/net/mutex.h"
 #include "tinyrpc/net/tinypb/tinypb_rpc_dispatcher.h"
 #include "tinyrpc/comm/log.h"
 #include "tinyrpc/comm/start.h"
 #include "test_tinypb_server.pb.h"
 
-#ifdef DECLARE_MYSQL_PLUGIN
 
-#include <mysql/mysql.h>
-#include "tinyrpc/comm/mysql_instase.h"
-
-#endif
-
+static int i = 0;
+tinyrpc::CoroutineMutex g_cor_mutex;
 
 class QueryServiceImpl : public QueryService {
  public:
@@ -35,55 +32,8 @@ class QueryServiceImpl : public QueryService {
     // sleep(6);
     // DebugLog << "sleep 6s end";
 
-    response->set_ret_code(0);
-    response->set_res_info("OK");
-
-    #ifdef DECLARE_MYSQL_PLUGIN
-
-    AppDebugLog << "install mysql pulgin, begin to query mysql";
-    tinyrpc::MySQLInstase::ptr instase =  tinyrpc::MySQLInstaseFactroy::GetThreadMySQLFactory()->GetMySQLInstase("test_db_key1");
-    if (!instase || !instase->isInitSuccess()) {
-      response->set_ret_code(-1);
-      response->set_res_info("faild to init mysql");
-      AppErrorLog << "mysql instase init failed";
-      return;
-    }
-
-    int n = 1000000;
-    // while(n--) {
-    char query_sql[512];
-    sprintf(query_sql, "select user_id, user_name, user_gender from user_db.t_user_information where user_id = '%s';", std::to_string(request->id()).c_str());
-
-    int rt = instase->query(std::string(query_sql));
-    if (rt != 0) {
-      response->set_ret_code(-1);
-      response->set_res_info(instase->getMySQLErrorInfo());
-      AppErrorLog << "failed to excute sql" << std::string(query_sql);
-      return;
-    }
-
-    MYSQL_RES* res = instase->storeResult();
-
-    MYSQL_ROW row = instase->fetchRow(res);
-    if (row) {
-      int i = 0;
-      AppDebugLog << "query success";
-      response->set_id(std::atoi(row[i++]));
-      response->set_name(std::string(row[i++]));
-    } else {
-      AppDebugLog << "query empty";
-      response->set_ret_code(-1);
-      response->set_res_info("this user not exist");
-    }
-    instase->freeResult(res);
-
-    #else
-
-    AppDebugLog << "no install mysql pulgin, now direct return default value";
     response->set_id(request->id());
     response->set_name("ikerli");
-
-    #endif
 
     AppInfoLog << "QueryServiceImpl.query_name, req={"<< request->ShortDebugString() << "}, res={" << response->ShortDebugString() << "}";
 
@@ -108,6 +58,13 @@ class QueryServiceImpl : public QueryService {
     response->set_req_no(request->req_no());
     response->set_id(request->id());
     response->set_age(100100111);
+
+    g_cor_mutex.lock();
+    AppDebugLog << "begin i = " << i;
+    sleep(1);
+    i++;
+    AppDebugLog << "end i = " << i;
+    g_cor_mutex.unlock();
 
     if (done) {
       done->Run();
