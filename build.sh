c3917dbf96e6f66444a8cc8344d7000d8bcf6d7b
ning2510 <1367560112@qq.com>
2023-03-15 13:56:42 +0800

    Add cmake build method

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-./build}
+
+PATH_INSTALL_INC_ROOT=${PATH_INSTALL_INC_ROOT:-/usr/include}
+PATH_INSTALL_LIB_ROOT=${PATH_INSTALL_LIB_ROOT:-/usr/lib}
+INCLUDE_DIR=${INCLUDE_DIR:-./include}
+LIB=${LIB:-./lib/libtinyrpc.a}
+
+mkdir -p ${BUILD_DIR} \
+    && mkdir -p ${INCLUDE_DIR} \
+    && cd ${BUILD_DIR} \
+    && cmake .. \
+    && make install \
+    && cd .. \
+    && cp -r ${INCLUDE_DIR}/tinyrpc ${PATH_INSTALL_INC_ROOT} \
+    && cp ${LIB} ${PATH_INSTALL_LIB_ROOT} \
+    && rm -rf ${INCLUDE_DIR}
+
