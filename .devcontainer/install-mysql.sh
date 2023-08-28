60e177959c0da303c36f06596e4ab99c52eccb3c
peidong.xu <peidong.xu@shopee.com>
2023-08-28 16:56:33 +0800

    fix devcontainer

--- .devcontainer/install-mysql.sh
+++ .devcontainer/install-mysql.sh
@@ -1,11 +1,13 @@
 echo "Building mysql connector"
 
 sudo apt-get update
-sudo apt-get install libssl-dev libmysqlclient-dev
+# sudo apt-get install libssl-dev libmysqlclient-dev
+sudo apt-get install libmysqlcppconn-dev
 wget https://dev.mysql.com/get/Downloads/Connector-C++/mysql-connector-c++-8.0.26-src.tar.gz
 tar -xvf mysql-connector-c++-8.0.26-src.tar.gz
 cd mysql-connector-c++-8.0.26-src
 cmake .
 make
 sudo make install
-rm ../mysql-connector-c++-8.0.26-src.tar.gz
+cd ..
+rm -rf /mysql-connector-c++-8.0.26-src.tar.gz
